15fa3bbdb86157a02f2094f19df54809794832ac
alialaba <alialaba079@gmail.com>
2024-10-04 15:58:21 -0400

    feat(models):add database models for user and product

---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    category: {type: String, required: true},
+    subcategory: {type: String, required: true},
+    sizes: {type: Array, required: true},
+    image: {type:[ String], required: true},
+    bestseller: {type: Boolean},
+    date: {type: Date, required: true}
+
+})
+
+const productModel = mongoose.model.product || mongoose.model("product", productSchema);
+export default productModel
