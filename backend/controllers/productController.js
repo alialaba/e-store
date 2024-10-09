54bc80db46e3cdb50cf9e7d28f7154f904eaf1d0
alialaba <alialaba079@gmail.com>
2024-10-09 16:01:36 -0400

    feat(middleware): implement admin middleware logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -81,7 +81,14 @@ const deleteProduct = async (req, res) => {
 
 const singleProduct = async (req, res) => {
   try {
-  } catch (error) {}
+    const {productId} = req.body;
+    const product = await productModel.findById(productId);
+
+    res.json({success: true, product})
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message})
+  }
 };
 
 export { addProduct, listProducts, deleteProduct, singleProduct };
